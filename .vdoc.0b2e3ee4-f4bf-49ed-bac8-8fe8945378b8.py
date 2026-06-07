5ddefda9ff01b5b5e03fb250ebe0c7de5b549ac7
Danilo Souza <danilosouza@usp.br>
2026-06-07 12:00:17 -0300

    Inclusão da versão inicial do capítulo 11

---
+++ .vdoc.0b2e3ee4-f4bf-49ed-bac8-8fe8945378b8.py
@@ -0,0 +1,89 @@
+# type: ignore
+# flake8: noqa
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| message: false
+#| warning: false
+
+import rich
+import dataclasses
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+x = 10
+y = 'Danilo'
+
+print(type(x))
+print(type(y))
+#
+#
+#
+#
+#
+#
+#
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
---
+++ .vdoc.64204fd8-c2aa-43b7-ba7d-2bce036b1b4a.py
@@ -0,0 +1,90 @@
+# type: ignore
+# flake8: noqa
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| message: false
+#| warning: false
+
+import rich
+import dataclasses
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+x = 10
+y = 'Danilo'
+
+print(type(x))
+print(type(y))
+#
+#
+#
+#
+#
+#
+#
+print(type(x))
+print(type(y))
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
